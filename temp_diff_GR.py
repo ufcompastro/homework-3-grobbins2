eeea078342b0f565799c038db3ad4f847e5d8417
Grady <grobbins1@ufl.edu>
2022-10-10 15:17:28 -0400

    loaded files to code and coverted to seperate variables

--- temp_diff_GR.py
+++ temp_diff_GR.py
@@ -1,11 +1,20 @@
+#Code by Grady Robbins
+#Worked with Bella Macias
+
 import numpy as np
 
-T_annual_avg = np.array([2,1],np.loadtxt("annual_csv_average.csv",float))
-T_annual = np.array(np.loadtxt("annual_csv.csv",float))
+T_annual_avgstart = np.loadtxt("annual_csv_average.csv",float,skiprows=1) # basic file import
+T_annualstart = np.loadtxt("annual_csv.csv",float, skiprows=1)
+T_annual_avg = np.reshape(T_annual_avgstart,(2,13)) #converting to preferred form
+T_annual = np.reshape(T_annualstart,(2,137))
 
+Time_annual = T_annual[0] #seperating data sets into temps and time
+Temp_annual = T_annual[1]
+Time_annual_avg = T_annual_avg[0]
+Temp_annual_avg = T_annual_avg[1]
 
 def ddx(xi,f,h):  #f is function, xi is initial point, h is step size                                                                                                      
 
     return ((f(xi+h) - f(xi-h))/(2*h))
 
-print(T_annual,T_annual_avg)
+#print(T_annual,T_annual_avg)
